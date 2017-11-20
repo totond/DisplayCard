f0b4f99011f4fb0d4762354470ce02040511796f
yany <yanzhikai_yjk@qq.com>
2017-11-20 19:13:32 +0800

    test pull

--- app/src/main/java/yanzhikai/example/MainActivity.java
+++ app/src/main/java/yanzhikai/example/MainActivity.java
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         dc = (DisplayCard) findViewById(R.id.dc);
 
         dc.setTextSize(16);
-        dc.setTittle("Hello");
-        dc.setText("Hi there! 1 parsec = 1,214,800,000,000,000,000 inches.");
+        dc.setTittle("How many inches in a parsec?");
+        dc.setText("1 parsec = 1,214,800,000,000,000,000 inches.");
         dc.loadPicture(this,"https://m.media-amazon.com/images/S/com.evi.images-irs/premium/a7/a7be6c563127224c91b57f2bb220d688._UX1024_SY600_.png");
     }
 }
