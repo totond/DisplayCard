f2b05db00084f758fa4d6dd4fa8badab40ea2801
yany <yanzhikai_yjk@qq.com>
2017-11-20 17:59:19 +0800

    完成有无图两种模式的接口

--- app/src/main/java/yanzhikai/example/MainActivity.java
+++ app/src/main/java/yanzhikai/example/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         dc = (DisplayCard) findViewById(R.id.dc);
 
-        dc.setTextSize(18);
+        dc.setTextSize(16);
         dc.setTittle("Hello");
         dc.setText("Hi there! 1 parsec = 1,214,800,000,000,000,000 inches.");
         dc.loadPicture(this,"https://m.media-amazon.com/images/S/com.evi.images-irs/premium/a7/a7be6c563127224c91b57f2bb220d688._UX1024_SY600_.png");
--- card/src/main/java/yanzhikai/card/DisplayCard.java
+++ card/src/main/java/yanzhikai/card/DisplayCard.java
@@ -1,6 +1,8 @@
 package yanzhikai.card;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.IntDef;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -10,11 +12,20 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by yany on 2017/11/20.
  */
 
-public class DisplayCard extends CardView{
+public class DisplayCard extends CardView {
+    public static final int NO_PICTURE = 0 , PICTURE = 1;
+    @IntDef({NO_PICTURE, PICTURE})
+    @Retention(RetentionPolicy.SOURCE)
+    public  @interface DisplayMode {}
+    private @DisplayMode int mDisplayMode = NO_PICTURE;
+
     private TextView tv_tittle, tv_text;
     private ImageView iv_picture;
 
@@ -25,16 +36,33 @@ public class DisplayCard extends CardView{
 
     public DisplayCard(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initAttrs(context, attrs);
         init(context);
     }
 
     public DisplayCard(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initAttrs(context, attrs);
         init(context);
     }
 
-    private void init(Context context){
-        LayoutInflater.from(context).inflate(R.layout.card_picture,this);
+    @SuppressWarnings("WrongConstant")
+    private void initAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DisplayCard, 0, 0);
+        mDisplayMode = typedArray.getInt(R.styleable.DisplayCard_mode,mDisplayMode);
+
+    }
+
+    private void init(Context context) {
+        switch (mDisplayMode){
+            case NO_PICTURE:
+                LayoutInflater.from(context).inflate(R.layout.card_without_picture, this);
+                break;
+            case PICTURE:
+                LayoutInflater.from(context).inflate(R.layout.card_picture, this);
+                break;
+        }
+
 
         tv_tittle = (TextView) findViewById(R.id.tv_tittle);
         tv_text = (TextView) findViewById(R.id.tv_text);
@@ -42,12 +70,14 @@ public class DisplayCard extends CardView{
 
     }
 
-    public void loadPicture(Context context, String url){
-        RequestOptions options = new RequestOptions();
-        Glide.with(context)
-                .load(url)
-                .apply(options.centerCrop())
-                .into(iv_picture);
+    public void loadPicture(Context context, String url) {
+        if (iv_picture != null) {
+            RequestOptions options = new RequestOptions();
+            Glide.with(context)
+                    .load(url)
+                    .apply(options.centerCrop())
+                    .into(iv_picture);
+        }
     }
 
     public void setText(String text) {
@@ -58,11 +88,19 @@ public class DisplayCard extends CardView{
         tv_tittle.setText(tittle);
     }
 
-    public void setTittleSize(int size){
+    public void setTittleSize(int size) {
         tv_tittle.setTextSize(size);
     }
 
-    public void setTextSize(int size){
+    public void setTextSize(int size) {
         tv_text.setTextSize(size);
+    }
+
+    public void setTittleColor(int color) {
+        tv_tittle.setTextColor(color);
+    }
+
+    public void setTextColor(int color) {
+        tv_text.setTextColor(color);
     }
 }
